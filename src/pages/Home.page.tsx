35cc154f27743742a749fe8f01b7574f1a3f8510
eruditezero <22434855+eruditezero@users.noreply.github.com>
2025-11-06 20:45:17 +0000

    Basic app shell

--- src/pages/Home.page.tsx
+++ src/pages/Home.page.tsx
@@ -1,11 +1,40 @@
-import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
-import { Welcome } from '../components/Welcome/Welcome';
+import { AppShell, Burger, NavLink } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
+import { IconHome2, IconGauge, IconChevronRight, IconActivity, IconCircleOff } from '@tabler/icons-react';
 
 export function HomePage() {
+  const [opened, { toggle }] = useDisclosure();
+
   return (
-    <>
-      <Welcome />
-      <ColorSchemeToggle />
-    </>
+    <AppShell
+      padding="md"
+      header={{ height: 60 }}
+      navbar={{
+        width: 300,
+        breakpoint: 'sm',
+        collapsed: { mobile: !opened },
+      }}
+    >
+      <AppShell.Header>
+        <Burger
+          opened={opened}
+          onClick={toggle}
+          hiddenFrom="sm"
+          size="sm"
+        />
+
+        <div>Logo</div>
+      </AppShell.Header>
+
+      <AppShell.Navbar>
+        <NavLink
+          href="#required-for-focus"
+          label="With icon"
+          leftSection={<IconHome2 size={16} stroke={1.5} />}
+        />
+      </AppShell.Navbar>
+
+      <AppShell.Main>Main</AppShell.Main>
+    </AppShell>
   );
 }
