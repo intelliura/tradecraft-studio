7b30e273fbbcc054b4d18fc4804d829b35d64070
eruditezero <22434855+eruditezero@users.noreply.github.com>
2025-11-06 21:10:17 +0000

    Navbar toggle, title

--- src/pages/Home.page.tsx
+++ src/pages/Home.page.tsx
@@ -1,4 +1,4 @@
-import { AppShell, Burger, NavLink } from '@mantine/core';
+import { AppShell, Badge, Burger, Flex, Group, NavLink, Text } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
 import { IconHome2, IconGauge, IconChevronRight, IconActivity, IconCircleOff } from '@tabler/icons-react';
 
@@ -12,18 +12,28 @@ export function HomePage() {
       navbar={{
         width: 300,
         breakpoint: 'sm',
-        collapsed: { mobile: !opened },
+        collapsed: { mobile: !opened, desktop: !opened },
       }}
     >
       <AppShell.Header>
-        <Burger
-          opened={opened}
-          onClick={toggle}
-          hiddenFrom="sm"
-          size="sm"
-        />
-
-        <div>Logo</div>
+        <Flex
+          mih={60}
+          pl="md"
+          gap="md"
+          justify="flex-start"
+          align="center"
+          direction="row"
+          wrap="wrap"
+        >
+          <Group>
+            <Burger
+              opened={opened}
+              onClick={toggle}
+              size="sm"
+            />
+            <Text>Tradecraft Studio</Text>
+          </Group>
+        </Flex>
       </AppShell.Header>
 
       <AppShell.Navbar>
@@ -31,6 +41,59 @@ export function HomePage() {
           href="#required-for-focus"
           label="With icon"
           leftSection={<IconHome2 size={16} stroke={1.5} />}
+        />
+        <NavLink
+          href="#required-for-focus"
+          label="With right section"
+          leftSection={<IconGauge size={16} stroke={1.5} />}
+          rightSection={
+            <IconChevronRight size={12} stroke={1.5} className="mantine-rotate-rtl" />
+          }
+        />
+        <NavLink
+          href="#required-for-focus"
+          label="Disabled"
+          leftSection={<IconCircleOff size={16} stroke={1.5} />}
+          disabled
+        />
+        <NavLink
+          href="#required-for-focus"
+          label="With description"
+          description="Additional information"
+          leftSection={
+            <Badge size="xs" color="red" circle>
+              3
+            </Badge>
+          }
+        />
+        <NavLink
+          href="#required-for-focus"
+          label="Active subtle"
+          leftSection={<IconActivity size={16} stroke={1.5} />}
+          rightSection={
+            <IconChevronRight size={12} stroke={1.5} className="mantine-rotate-rtl" />
+          }
+          variant="subtle"
+          active
+        />
+        <NavLink
+          href="#required-for-focus"
+          label="Active light"
+          leftSection={<IconActivity size={16} stroke={1.5} />}
+          rightSection={
+            <IconChevronRight size={12} stroke={1.5} className="mantine-rotate-rtl" />
+          }
+          active
+        />
+        <NavLink
+          href="#required-for-focus"
+          label="Active filled"
+          leftSection={<IconActivity size={16} stroke={1.5} />}
+          rightSection={
+            <IconChevronRight size={12} stroke={1.5} className="mantine-rotate-rtl" />
+          }
+          variant="filled"
+          active
         />
       </AppShell.Navbar>
 
